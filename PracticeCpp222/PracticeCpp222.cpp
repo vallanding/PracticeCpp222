dda3e2244bd1e9a6ef9ef13d036ca051204528e0
vallanding <Ilya770022@gmail.com>
2025-11-07 23:50:37 +0500

    Update PracticeCpp222.cpp
    rename

--- PracticeCpp222/PracticeCpp222.cpp
+++ PracticeCpp222/PracticeCpp222.cpp
@@ -1,328 +1,311 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <memory>
 #include <vector>
 #include <stdexcept>
 #include <cctype>
 
-// Базовый интерфейс сериализатора
 class Serializer {
 public:
     virtual ~Serializer() = default;
 
-    virtual void add_field(const std::string& name, const std::string& value) = 0;
-    virtual void add_field(const std::string& name, int value) = 0;
-    virtual void add_field(const std::string& name, double value) = 0;
+    virtual void addField(const std::string& fieldName, const std::string& fieldValue) = 0;
+    virtual void addField(const std::string& fieldName, int fieldValue) = 0;
+    virtual void addField(const std::string& fieldName, double fieldValue) = 0;
 
-    virtual void add_block(const std::string& name) = 0;
-    virtual void end_block() = 0;
+    virtual void addBlock(const std::string& blockName) = 0;
+    virtual void endBlock() = 0;
 
     virtual std::string build() = 0;
 };
 
-// XML сериализатор
 class XmlSerializer : public Serializer {
 private:
-    std::vector<std::string> blocks_;
-    std::string content_;
-    int indent_level_ = 0;
+    std::vector<std::string> blockStack;
+    std::string outputContent;
+    int currentIndentLevel = 0;
 
-    std::string get_indent() const {
-        return std::string(indent_level_ * 2, ' ');
+    std::string getCurrentIndent() const {
+        return std::string(currentIndentLevel * 2, ' ');
     }
 
 public:
-    void add_field(const std::string& name, const std::string& value) override {
-        content_ += get_indent() + "<" + name + ">" + value + "</" + name + ">\n";
+    void addField(const std::string& fieldName, const std::string& fieldValue) override {
+        outputContent += getCurrentIndent() + "<" + fieldName + ">" + fieldValue + "</" + fieldName + ">\n";
     }
 
-    void add_field(const std::string& name, int value) override {
-        content_ += get_indent() + "<" + name + ">" + std::to_string(value) + "</" + name + ">\n";
+    void addField(const std::string& fieldName, int fieldValue) override {
+        outputContent += getCurrentIndent() + "<" + fieldName + ">" + std::to_string(fieldValue) + "</" + fieldName + ">\n";
     }
 
-    void add_field(const std::string& name, double value) override {
-        content_ += get_indent() + "<" + name + ">" + std::to_string(value) + "</" + name + ">\n";
+    void addField(const std::string& fieldName, double fieldValue) override {
+        outputContent += getCurrentIndent() + "<" + fieldName + ">" + std::to_string(fieldValue) + "</" + fieldName + ">\n";
     }
 
-    void add_block(const std::string& name) override {
-        content_ += get_indent() + "<" + name + ">\n";
-        blocks_.push_back(name);
-        indent_level_++;
+    void addBlock(const std::string& blockName) override {
+        outputContent += getCurrentIndent() + "<" + blockName + ">\n";
+        blockStack.push_back(blockName);
+        currentIndentLevel++;
     }
 
-    void end_block() override {
-        if (!blocks_.empty()) {
-            indent_level_--;
-            std::string block_name = blocks_.back();
-            blocks_.pop_back();
-            content_ += get_indent() + "</" + block_name + ">\n";
+    void endBlock() override {
+        if (!blockStack.empty()) {
+            currentIndentLevel--;
+            std::string lastBlockName = blockStack.back();
+            blockStack.pop_back();
+            outputContent += getCurrentIndent() + "</" + lastBlockName + ">\n";
         }
     }
 
     std::string build() override {
-        while (!blocks_.empty()) {
-            end_block();
+        while (!blockStack.empty()) {
+            endBlock();
         }
-        return content_;
+        return outputContent;
     }
 };
 
-// JSON сериализатор
 class JsonSerializer : public Serializer {
 private:
-    std::vector<std::string> blocks_;
-    std::string content_;
-    bool needs_comma_ = false;
-    int indent_level_ = 0;
+    std::vector<std::string> blockStack;
+    std::string outputContent;
+    bool isCommaNeeded = false;
+    int currentIndentLevel = 0;
 
-    std::string get_indent() const {
-        return std::string(indent_level_ * 2, ' ');
+    std::string getCurrentIndent() const {
+        return std::string(currentIndentLevel * 2, ' ');
     }
 
-    void handle_comma() {
-        if (needs_comma_) {
-            content_ += ",";
+    void handleCommaIfNeeded() {
+        if (isCommaNeeded) {
+            outputContent += ",";
         }
-        content_ += "\n";
-        needs_comma_ = true;
+        outputContent += "\n";
+        isCommaNeeded = true;
     }
 
 public:
-    void add_field(const std::string& name, const std::string& value) override {
-        handle_comma();
-        content_ += get_indent() + "\"" + name + "\": \"" + value + "\"";
+    void addField(const std::string& fieldName, const std::string& fieldValue) override {
+        handleCommaIfNeeded();
+        outputContent += getCurrentIndent() + "\"" + fieldName + "\": \"" + fieldValue + "\"";
     }
 
-    void add_field(const std::string& name, int value) override {
-        handle_comma();
-        content_ += get_indent() + "\"" + name + "\": " + std::to_string(value);
+    void addField(const std::string& fieldName, int fieldValue) override {
+        handleCommaIfNeeded();
+        outputContent += getCurrentIndent() + "\"" + fieldName + "\": " + std::to_string(fieldValue);
     }
 
-    void add_field(const std::string& name, double value) override {
-        handle_comma();
-        content_ += get_indent() + "\"" + name + "\": " + std::to_string(value);
+    void addField(const std::string& fieldName, double fieldValue) override {
+        handleCommaIfNeeded();
+        outputContent += getCurrentIndent() + "\"" + fieldName + "\": " + std::to_string(fieldValue);
     }
 
-    void add_block(const std::string& name) override {
-        handle_comma();
-        content_ += get_indent() + "\"" + name + "\": {";
-        blocks_.push_back(name);
-        indent_level_++;
-        needs_comma_ = false;
+    void addBlock(const std::string& blockName) override {
+        handleCommaIfNeeded();
+        outputContent += getCurrentIndent() + "\"" + blockName + "\": {";
+        blockStack.push_back(blockName);
+        currentIndentLevel++;
+        isCommaNeeded = false;
     }
 
-    void end_block() override {
-        if (!blocks_.empty()) {
-            indent_level_--;
-            content_ += "\n" + get_indent() + "}";
-            blocks_.pop_back();
-            needs_comma_ = true;
+    void endBlock() override {
+        if (!blockStack.empty()) {
+            currentIndentLevel--;
+            outputContent += "\n" + getCurrentIndent() + "}";
+            blockStack.pop_back();
+            isCommaNeeded = true;
         }
     }
 
     std::string build() override {
-        while (!blocks_.empty()) {
-            end_block();
+        while (!blockStack.empty()) {
+            endBlock();
         }
-        return "{\n" + content_ + "\n}";
+        return "{\n" + outputContent + "\n}";
     }
 };
 
-// Базовый класс Vehicle
 class Vehicle {
 public:
     virtual ~Vehicle() = default;
     virtual void serialize(Serializer& serializer) const = 0;
 
-    std::string name;
-    std::string manufacturer;
-    double weight;
-    double power;
-    int year;
+    std::string modelName;
+    std::string manufacturerName;
+    double vehicleWeight;
+    double enginePower;
+    int productionYear;
 };
 
-// Автомобиль
 class Car : public Vehicle {
 public:
-    int doors;
-    int passenger_seats;
-    std::string fuel_type;
-    double engine_volume;
+    int doorCount;
+    int passengerSeatCount;
+    std::string fuelType;
+    double engineVolume;
 
     void serialize(Serializer& serializer) const override {
-        serializer.add_block("vehicle");
-        serializer.add_field("type", "Car");
-        serializer.add_field("name", name);
-        serializer.add_field("manufacturer", manufacturer);
-        serializer.add_field("weight", weight);
-        serializer.add_field("power", power);
-        serializer.add_field("year", year);
+        serializer.addBlock("vehicle");
+        serializer.addField("type", "Car");
+        serializer.addField("name", modelName);
+        serializer.addField("manufacturer", manufacturerName);
+        serializer.addField("weight", vehicleWeight);
+        serializer.addField("power", enginePower);
+        serializer.addField("year", productionYear);
 
-        serializer.add_block("car_specific");
-        serializer.add_field("doors", doors);
-        serializer.add_field("passenger_seats", passenger_seats);
-        serializer.add_field("fuel_type", fuel_type);
-        serializer.add_field("engine_volume", engine_volume);
-        serializer.end_block();
+        serializer.addBlock("carSpecific");
+        serializer.addField("doors", doorCount);
+        serializer.addField("passengerSeats", passengerSeatCount);
+        serializer.addField("fuelType", fuelType);
+        serializer.addField("engineVolume", engineVolume);
+        serializer.endBlock();
 
-        serializer.end_block();
+        serializer.endBlock();
     }
 };
 
-// Самолет
 class Airplane : public Vehicle {
 public:
-    int wingspan;
-    int max_altitude;
-    int passenger_capacity;
-    double max_speed;
+    int wingSpan;
+    int maxAltitude;
+    int maxPassengerCapacity;
+    double maxSpeed;
 
     void serialize(Serializer& serializer) const override {
-        serializer.add_block("vehicle");
-        serializer.add_field("type", "Airplane");
-        serializer.add_field("name", name);
-        serializer.add_field("manufacturer", manufacturer);
-        serializer.add_field("weight", weight);
-        serializer.add_field("power", power);
-        serializer.add_field("year", year);
+        serializer.addBlock("vehicle");
+        serializer.addField("type", "Airplane");
+        serializer.addField("name", modelName);
+        serializer.addField("manufacturer", manufacturerName);
+        serializer.addField("weight", vehicleWeight);
+        serializer.addField("power", enginePower);
+        serializer.addField("year", productionYear);
 
-        serializer.add_block("airplane_specific");
-        serializer.add_field("wingspan", wingspan);
-        serializer.add_field("max_altitude", max_altitude);
-        serializer.add_field("passenger_capacity", passenger_capacity);
-        serializer.add_field("max_speed", max_speed);
-        serializer.end_block();
+        serializer.addBlock("airplaneSpecific");
+        serializer.addField("wingspan", wingSpan);
+        serializer.addField("maxAltitude", maxAltitude);
+        serializer.addField("passengerCapacity", maxPassengerCapacity);
+        serializer.addField("maxSpeed", maxSpeed);
+        serializer.endBlock();
 
-        serializer.end_block();
+        serializer.endBlock();
     }
 };
 
-// Корабль
 class Ship : public Vehicle {
 public:
-    double length;
-    double displacement;
-    int crew_capacity;
-    std::string propulsion_type;
+    double shipLength;
+    double shipDisplacement;
+    int crewCapacity;
+    std::string propulsionType;
 
     void serialize(Serializer& serializer) const override {
-        serializer.add_block("vehicle");
-        serializer.add_field("type", "Ship");
-        serializer.add_field("name", name);
-        serializer.add_field("manufacturer", manufacturer);
-        serializer.add_field("weight", weight);
-        serializer.add_field("power", power);
-        serializer.add_field("year", year);
+        serializer.addBlock("vehicle");
+        serializer.addField("type", "Ship");
+        serializer.addField("name", modelName);
+        serializer.addField("manufacturer", manufacturerName);
+        serializer.addField("weight", vehicleWeight);
+        serializer.addField("power", enginePower);
+        serializer.addField("year", productionYear);
 
-        serializer.add_block("ship_specific");
-        serializer.add_field("length", length);
-        serializer.add_field("displacement", displacement);
-        serializer.add_field("crew_capacity", crew_capacity);
-        serializer.add_field("propulsion_type", propulsion_type);
-        serializer.end_block();
+        serializer.addBlock("shipSpecific");
+        serializer.addField("length", shipLength);
+        serializer.addField("displacement", shipDisplacement);
+        serializer.addField("crewCapacity", crewCapacity);
+        serializer.addField("propulsionType", propulsionType);
+        serializer.endBlock();
 
-        serializer.end_block();
+        serializer.endBlock();
     }
 };
 
-// Функция сериализации
-void serialize(const Vehicle& vehicle, Serializer& serializer) {
+void serializeVehicle(const Vehicle& vehicle, Serializer& serializer) {
     vehicle.serialize(serializer);
 }
 
-// Фабрика для создания сериализаторов
-std::unique_ptr<Serializer> create_serializer(const std::string& format) {
-    if (format == "xml") {
+std::unique_ptr<Serializer> createSerializer(const std::string& outputFormat) {
+    if (outputFormat == "xml") {
         return std::make_unique<XmlSerializer>();
     }
-    else if (format == "json") {
+    else if (outputFormat == "json") {
         return std::make_unique<JsonSerializer>();
     }
-    throw std::invalid_argument("Unsupported format: " + format);
+    throw std::invalid_argument("Unsupported format: " + outputFormat);
 }
 
-// Функция для преобразования строки в верхний регистр
-std::string to_uppercase(const std::string& str) {
-    std::string result = str;
-    for (char& c : result) {
-        c = std::toupper(c);
+std::string toUpperCase(const std::string& inputString) {
+    std::string resultString = inputString;
+    for (char& currentChar : resultString) {
+        currentChar = std::toupper(currentChar);
     }
-    return result;
+    return resultString;
 }
 
 int main() {
-    std::string format;
+    std::string selectedFormat;
 
-    // Спрашиваем у пользователя формат
     std::cout << "Choose format (json/xml): ";
-    std::cin >> format;
+    std::cin >> selectedFormat;
 
-    // Приводим к нижнему регистру для проверки
-    for (char& c : format) {
-        c = std::tolower(c);
+    for (char& currentChar : selectedFormat) {
+        currentChar = std::tolower(currentChar);
     }
 
-    if (format != "json" && format != "xml") {
+    if (selectedFormat != "json" && selectedFormat != "xml") {
         std::cout << "Invalid format. Using JSON by default.\n";
-        format = "json";
+        selectedFormat = "json";
     }
 
     try {
-        auto serializer = create_serializer(format);
+        auto formatSerializer = createSerializer(selectedFormat);
 
-        // Создаем транспортные средства
-        Car bmw;
-        bmw.name = "BMW G30";
-        bmw.manufacturer = "BMW";
-        bmw.weight = 1600;
-        bmw.power = 252;
-        bmw.year = 2020;
-        bmw.doors = 4;
-        bmw.passenger_seats = 5;
-        bmw.fuel_type = "petrol";
-        bmw.engine_volume = 2.0;
+        Car bmwCar;
+        bmwCar.modelName = "BMW G30";
+        bmwCar.manufacturerName = "BMW";
+        bmwCar.vehicleWeight = 1600;
+        bmwCar.enginePower = 252;
+        bmwCar.productionYear = 2020;
+        bmwCar.doorCount = 4;
+        bmwCar.passengerSeatCount = 5;
+        bmwCar.fuelType = "petrol";
+        bmwCar.engineVolume = 2.0;
 
-        Airplane boeing;
-        boeing.name = "Boeing 747-400";
-        boeing.manufacturer = "Boeing";
-        boeing.weight = 180000;
-        boeing.power = 240000;
-        boeing.year = 1988;
-        boeing.wingspan = 64;
-        boeing.max_altitude = 13700;
-        boeing.passenger_capacity = 416;
-        boeing.max_speed = 988;
+        Airplane boeingPlane;
+        boeingPlane.modelName = "Boeing 747-400";
+        boeingPlane.manufacturerName = "Boeing";
+        boeingPlane.vehicleWeight = 180000;
+        boeingPlane.enginePower = 240000;
+        boeingPlane.productionYear = 1988;
+        boeingPlane.wingSpan = 64;
+        boeingPlane.maxAltitude = 13700;
+        boeingPlane.maxPassengerCapacity = 416;
+        boeingPlane.maxSpeed = 988;
 
-        Ship queen_victoria;
-        queen_victoria.name = "MS Queen Victoria";
-        queen_victoria.manufacturer = "Fincantieri";
-        queen_victoria.weight = 90000000;
-        queen_victoria.power = 120000;
-        queen_victoria.year = 2007;
-        queen_victoria.length = 294;
-        queen_victoria.displacement = 90000;
-        queen_victoria.crew_capacity = 1000;
-        queen_victoria.propulsion_type = "diesel-electric";
+        Ship victoriaShip;
+        victoriaShip.modelName = "MS Queen Victoria";
+        victoriaShip.manufacturerName = "Fincantieri";
+        victoriaShip.vehicleWeight = 90000000;
+        victoriaShip.enginePower = 120000;
+        victoriaShip.productionYear = 2007;
+        victoriaShip.shipLength = 294;
+        victoriaShip.shipDisplacement = 90000;
+        victoriaShip.crewCapacity = 1000;
+        victoriaShip.propulsionType = "diesel-electric";
 
-        // Сериализуем все транспортные средства
-        std::vector<Vehicle*> vehicles = { &bmw, &boeing, &queen_victoria };
+        std::vector<Vehicle*> vehicleList = { &bmwCar, &boeingPlane, &victoriaShip };
 
-        std::cout << "\n=== Serialized vehicles in " << to_uppercase(format) << " format ===\n\n";
+        std::cout << "\n=== Serialized vehicles in " << toUpperCase(selectedFormat) << " format ===\n\n";
 
-        for (auto vehicle : vehicles) {
-            serialize(*vehicle, *serializer);
-            std::cout << serializer->build() << "\n\n";
-            // Сбрасываем сериализатор для следующего vehicle
-            serializer = create_serializer(format);
+        for (auto currentVehicle : vehicleList) {
+            serializeVehicle(*currentVehicle, *formatSerializer);
+            std::cout << formatSerializer->build() << "\n\n";
+            formatSerializer = createSerializer(selectedFormat);
         }
 
     }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+    catch (const std::exception& error) {
+        std::cerr << "Error: " << error.what() << std::endl;
         return 1;
     }
 
     return 0;
 }
-
-
